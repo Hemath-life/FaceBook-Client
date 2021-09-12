1a477be566b39a0dbbbca28d704c43667eb84e12
GoHem <hemath@inotesindia.com>
2021-09-12 18:33:19 +0530

    V -0.3 -Righbar added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,116 @@
+import "./rightbar.scss"
 const RightBar = () => {
-	return <div className=''>Right bar</div>
+	return (
+		<div className='rightBar'>
+			<div className='birthdayContainer d-flex justify-content-center align-items-center'>
+				<img src='assets/gift.png' alt='' />
+				<span className='text-capitalize ms-2 '>
+					<b>polo poster</b> and <b>3 other friends</b> have birthday today
+				</span>
+			</div>
+			<div className='rightBarAd'>
+				<img src='assets/ad.png ' alt='' />
+				<div className='onlineFriends'>
+					<span>
+						<b>Online Friends</b>
+					</span>
+					<ul className='p-0 d-flex  mt-2 flex-column'>
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>{" "}
+						<li className='mt-2'>
+							<img src='assets/person/10.jpeg' alt='' />
+							<span className='online'></span>
+							<span className='ms-2'>Justin Olivia</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+			.
+		</div>
+	)
 }
 
 export default RightBar
